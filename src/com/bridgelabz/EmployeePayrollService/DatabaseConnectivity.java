98a8dac0b2b1ac823d4dec291a47c6b79022e765
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-07-09 16:20:24 +0530

    Added implemented program for employee payroll service.

---
+++ src/com/bridgelabz/EmployeePayrollService/DatabaseConnectivity.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.EmployeePayrollService;
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class DatabaseConnectivity {
+
+    public static void main(String[] args)  {
+
+        try {
+                Class.forName("com.mysql.cj.jdbc.Driver") ;
+                System.out.println("Driver class loaded and registered");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service? user=root & password=root");
+                System.out.println("Connection established with dbserver");
+                } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/bridgelabz/EmployeePayrollService/EmployeePayrollData.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.EmployeePayrollService;
+
+public class EmployeePayrollData {
+    public EmployeePayrollData(int id, String name, double salary) {
+    }
+}
